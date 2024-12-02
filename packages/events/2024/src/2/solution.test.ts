6cb053ca631821147fc0c9b205eff8b20a79b6dc
conor-ob <conor.w.ob@gmail.com>
2024-12-02 20:59:55 +0000

    test

--- packages/events/2024/src/2/solution.test.ts
+++ packages/events/2024/src/2/solution.test.ts
@@ -1,21 +1,19 @@
-import { describe, expect, test } from "vitest";
-
-import { part1, part2 } from "./solution";
+import { describe, test } from "vitest";
 
 describe("day 2", () => {
   test("part 1 example", () => {
-    expect(part1("./src/2/input.example.txt")).toBe(2);
+    // expect(part1("./src/2/example.txt")).toBe(1);
   });
 
-  // test("part 1", () => {
-  //   expect(part1("./src/2/input.part1.txt")).toBe(71);
-  // });
+  test("part 1", () => {
+    // expect(part1("./src/2/input.part1.txt")).toBe(71);
+  });
 
-  // test("part 2 example", () => {
-  //   expect(part2("./src/2/input.example.txt")).toBe(31);
-  // });
+  test("part 2 example", () => {
+    // expect(part2("./src/2/input.example.txt")).toBe(31);
+  });
 
-  // test("part 2", () => {
-  //   expect(part2("./src/2/input.part1.txt")).toBe(22776016);
-  // });
+  test("part 2", () => {
+    // expect(part2("./src/2/input.part1.txt")).toBe(22776016);
+  });
 });
