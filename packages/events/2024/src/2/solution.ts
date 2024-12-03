3d309b796b7025adbafccf7b4fb798aa5b22791b
Conor O'Brien <conor.w.ob@gmail.com>
2024-12-03 00:06:41 +0000

    feat(2024): day 2 (#3)

--- packages/events/2024/src/2/solution.ts
+++ packages/events/2024/src/2/solution.ts
@@ -1,24 +1,93 @@
 export function part1(input: string) {
-  const reports = input.split("\n").map((line) => line.split(" "));
+  const lines = input.split("\n");
 
-  const safe = reports.reduce((acc, levels) => {
-    for (let i = 0; i < levels.length; i++) {
-      const current = Number(levels[i]);
-      const next = Number(levels[i + 1]);
+  const safe = lines.reduce((acc, line) => {
+    const report = line.split(" ").map(Number);
+    let state = "";
+    for (let i = 0; i < report.length; i++) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const current = report[i]!;
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const next = report[i + 1]!;
 
-      const diff = Math.abs(next - current);
-      if (diff > 2) {
+      if (current === next) {
         return acc;
+      }
+
+      if (Math.abs(next - current) > 3) {
+        return acc;
+      }
+
+      if (next > current) {
+        const previousState = state;
+        state = "increasing";
+        if (previousState === "decreasing") {
+          return acc;
+        }
+      }
+
+      if (next < current) {
+        const previousState = state;
+        state = "decreasing";
+        if (previousState === "increasing") {
+          return acc;
+        }
       }
     }
     return acc + 1;
   }, 0);
 
-  void safe;
-  return "TODO";
+  return safe;
 }
 
 export function part2(input: string) {
-  void input;
-  return "TODO";
+  function isSafe(report: number[]) {
+    let state = "";
+    for (let i = 0; i < report.length; i++) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const current = report[i]!;
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const next = report[i + 1]!;
+
+      if (current === next) {
+        return false;
+      }
+
+      if (Math.abs(next - current) > 3) {
+        return false;
+      }
+
+      if (next > current) {
+        const previousState = state;
+        state = "increasing";
+        if (previousState === "decreasing") {
+          return false;
+        }
+      }
+
+      if (next < current) {
+        const previousState = state;
+        state = "decreasing";
+        if (previousState === "increasing") {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  const lines = input.split("\n");
+  const safe = lines.reduce((acc, line) => {
+    const report = line.split(" ").map(Number);
+    for (let i = 0; i < report.length; i++) {
+      const newReport = [...report];
+      newReport.splice(i, 1);
+      if (isSafe(newReport)) {
+        return acc + 1;
+      }
+    }
+    return acc;
+  }, 0);
+
+  return safe;
 }
