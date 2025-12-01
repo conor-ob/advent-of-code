3f88fe3554f3ba18bf8244ce16813a86f5dc4cb7
conor-ob <conor.w.ob@gmail.com>
2025-12-01 20:08:30 +0000

    part 2

--- packages/events/2025/src/1/solution.ts
+++ packages/events/2025/src/1/solution.ts
@@ -16,25 +16,28 @@ export function part2(input: string) {
 }
 
 function applyInstructions(instructions: string[]) {
-  return instructions.reduce((acc, instruction) => {
-    const direction = instruction[0];
-    const distance = parseInt(instruction.slice(1));
-    
-    let newPosition = acc.position;
-    switch (direction) {
-      case "L":
-        newPosition = ((newPosition - distance) % 100 + 100) % 100;
-        break;
-      case "R":
-        newPosition = ((newPosition + distance) % 100 + 100) % 100;
-        break;
-      default:
-        throw new Error(`Invalid direction: ${direction}`);
-    }
-    if (newPosition === 0) {
-      return { ...acc, position: newPosition, password: acc.password + 1 };
-    } else {
-      return { ...acc, position: newPosition };
-    }
-  }, { position: 50, password: 0 });
+  return instructions.reduce(
+    (acc, instruction) => {
+      const direction = instruction[0];
+      const distance = parseInt(instruction.slice(1));
+
+      let newPosition = acc.position;
+      switch (direction) {
+        case "L":
+          newPosition = (((newPosition - distance) % 100) + 100) % 100;
+          break;
+        case "R":
+          newPosition = (((newPosition + distance) % 100) + 100) % 100;
+          break;
+        default:
+          throw new Error(`Invalid direction: ${direction}`);
+      }
+      if (newPosition === 0) {
+        return { ...acc, position: newPosition, password: acc.password + 1 };
+      } else {
+        return { ...acc, position: newPosition };
+      }
+    },
+    { position: 50, password: 0 },
+  );
 }
