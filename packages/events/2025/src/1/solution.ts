59d3f8123f63ac6a7e7a79a840bd5a2adb648d47
Conor O'Brien <conor.w.ob@gmail.com>
2025-12-01 20:12:27 +0000

    feat(2025): day 1 (#6)

---
+++ packages/events/2025/src/1/solution.ts
@@ -0,0 +1,43 @@
+/**
+ * https://adventofcode.com/2025/day/1
+ */
+export function part1(input: string) {
+  const instructions = input.split("\n");
+  return applyInstructions(instructions).password;
+}
+
+export function part2(input: string) {
+  const instructions = input.split("\n").flatMap((instruction) => {
+    const direction = instruction[0];
+    const distance = parseInt(instruction.slice(1));
+    return Array.from({ length: distance }, (_) => `${direction}1`);
+  });
+  return applyInstructions(instructions).password;
+}
+
+function applyInstructions(instructions: string[]) {
+  return instructions.reduce(
+    (acc, instruction) => {
+      const direction = instruction[0];
+      const distance = parseInt(instruction.slice(1));
+
+      let newPosition = acc.position;
+      switch (direction) {
+        case "L":
+          newPosition = (((newPosition - distance) % 100) + 100) % 100;
+          break;
+        case "R":
+          newPosition = (((newPosition + distance) % 100) + 100) % 100;
+          break;
+        default:
+          throw new Error(`Invalid direction: ${direction}`);
+      }
+      if (newPosition === 0) {
+        return { ...acc, position: newPosition, password: acc.password + 1 };
+      } else {
+        return { ...acc, position: newPosition };
+      }
+    },
+    { position: 50, password: 0 },
+  );
+}
