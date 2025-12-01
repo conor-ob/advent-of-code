891043e1b7eb4140ba1ab826fda5b70013d09274
conor-ob <conor.w.ob@gmail.com>
2025-12-01 19:59:59 +0000

    part 2

--- packages/events/2025/src/1/solution.test.ts
+++ packages/events/2025/src/1/solution.test.ts
@@ -6,10 +6,10 @@ import { part1, part2 } from "./solution";
 
 describe("day 1", () => {
   test("part 1", () => {
-    expect(part1(readFile("./src/1/input.txt"))).toBe("TODO");
+    expect(part1(readFile("./src/1/input.txt"))).toBe(982);
   });
 
   test("part 2", () => {
-    expect(part2(readFile("./src/1/input.txt"))).toBe("TODO");
+    expect(part2(readFile("./src/1/input.txt"))).toBe(6106);
   });
 });
--- packages/events/2025/src/1/solution.ts
+++ packages/events/2025/src/1/solution.ts
@@ -2,35 +2,23 @@
  * https://adventofcode.com/2025/day/1
  */
 export function part1(input: string) {
-  const lines = input.split("\n");
-  return lines.reduce((acc, line) => {
-    const direction = line[0];
-    const distance = parseInt(line.slice(1));
-    
-    let newPosition = acc.position;
-    switch (direction) {
-      case "L":
-        newPosition = ((newPosition - distance) % 100 + 100) % 100;
-        break;
-      case "R":
-        newPosition = ((newPosition + distance) % 100 + 100) % 100;
-        break;
-      default:
-        throw new Error(`Invalid direction: ${direction}`);
-    }
-    if (newPosition === 0) {
-      return { ...acc, position: newPosition, password: acc.password + 1 };
-    } else {
-      return { ...acc, position: newPosition };
-    }
-  }, { position: 50, password: 0 }).password;
+  const instructions = input.split("\n");
+  return applyInstructions(instructions).password;
 }
 
 export function part2(input: string) {
-  const lines = input.split("\n");
-  return lines.reduce((acc, line) => {
-    const direction = line[0];
-    const distance = parseInt(line.slice(1));
+  const instructions = input.split("\n").flatMap((instruction) => {
+    const direction = instruction[0];
+    const distance = parseInt(instruction.slice(1));
+    return Array.from({ length: distance }, (_) => `${direction}1`);
+  });
+  return applyInstructions(instructions).password;
+}
+
+function applyInstructions(instructions: string[]) {
+  return instructions.reduce((acc, instruction) => {
+    const direction = instruction[0];
+    const distance = parseInt(instruction.slice(1));
     
     let newPosition = acc.position;
     switch (direction) {
@@ -48,5 +36,5 @@ export function part2(input: string) {
     } else {
       return { ...acc, position: newPosition };
     }
-  }, { position: 50, password: 0 }).password;
+  }, { position: 50, password: 0 });
 }
