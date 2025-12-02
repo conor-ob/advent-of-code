b9ee0921fa578887158c7be2c561b361a5984442
conor-ob <conor.w.ob@gmail.com>
2025-12-02 18:33:35 +0000

    tests

--- packages/events/2025/src/2/solution.test.ts
+++ packages/events/2025/src/2/solution.test.ts
@@ -6,10 +6,10 @@ import { part1, part2 } from "./solution";
 
 describe("day 2", () => {
   test("part 1", () => {
-    expect(part1(readFile("./src/2/input.txt"))).toBe("TODO");
+    expect(part1(readFile("./src/2/input.txt"))).toBe(43952536386);
   });
 
   test("part 2", () => {
-    expect(part2(readFile("./src/2/input.txt"))).toBe("TODO");
+    expect(part2(readFile("./src/2/input.txt"))).toBe(54486209192);
   });
 });
