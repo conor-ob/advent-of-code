9742c027e88a298e876da460647b2813eea0b1ba
conor-ob <conor.w.ob@gmail.com>
2025-12-06 22:50:06 +0000

    blah

--- packages/events/2025/src/4/solution.ts
+++ packages/events/2025/src/4/solution.ts
@@ -5,43 +5,7 @@ export function part1(input: string) {
   const lines = input.split("\n");
   const lineLength = lines[0]?.length ?? 0;
   const positions = input.split("\n").flatMap((line) => line.split(""));
-  let accessible = 0;
-  for (let i = 0; i < positions.length; i++) {
-    if (positions[i] === "@") {
-      const check1 =
-        i % lineLength === 0 ? undefined : positions[i - lineLength - 1];
-      const check2 = positions[i - lineLength];
-      const check3 =
-        i % lineLength === lineLength - 1
-          ? undefined
-          : positions[i - lineLength + 1];
-      const check4 = i % lineLength === 0 ? undefined : positions[i - 1];
-      const check5 =
-        i % lineLength === lineLength - 1 ? undefined : positions[i + 1];
-      const check6 =
-        i % lineLength === 0 ? undefined : positions[i + lineLength - 1];
-      const check7 = positions[i + lineLength];
-      const check8 =
-        i % lineLength === lineLength - 1
-          ? undefined
-          : positions[i + lineLength + 1];
-
-      const checks = [
-        check1,
-        check2,
-        check3,
-        check4,
-        check5,
-        check6,
-        check7,
-        check8,
-      ];
-      if (checks.filter((it) => it === "@").length < 4) {
-        accessible = accessible + 1;
-      }
-    }
-  }
-  return accessible;
+  return removeRolls(positions, lineLength).removedRolls;
 }
 
 export function part2(input: string) {
@@ -49,22 +13,21 @@ export function part2(input: string) {
   const lineLength = lines[0]?.length ?? 0;
   let positions = input.split("\n").flatMap((line) => line.split(""));
 
-  let total = 0;
-  let each = 1;
-  while (each > 0) {
-    const { removedRolls: removed, positions: newPositions } = removedRolls(
-      positions,
-      lineLength,
-    );
+  let totalRemovedRolls = 0;
+  while (true) {
+    const { removedRolls, newPositions } = removeRolls(positions, lineLength);
+    totalRemovedRolls += removedRolls;
     positions = newPositions;
-    total = total + removed;
-    each = removed;
+
+    if (removedRolls === 0) {
+      break;
+    }
   }
-  return total;
+  return totalRemovedRolls;
 }
 
-function removedRolls(positions: string[], lineLength: number) {
-  let accessible = 0;
+function removeRolls(positions: string[], lineLength: number) {
+  let removedRolls = 0;
   const newPositions = [...positions];
   for (let i = 0; i < positions.length; i++) {
     if (positions[i] === "@") {
@@ -97,10 +60,10 @@ function removedRolls(positions: string[], lineLength: number) {
         check8,
       ];
       if (checks.filter((it) => it === "@").length < 4) {
-        accessible = accessible + 1;
+        removedRolls++;
         newPositions[i] = "x";
       }
     }
   }
-  return { removedRolls: accessible, positions: newPositions };
+  return { removedRolls, newPositions };
 }
