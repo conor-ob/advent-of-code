9cf9a33bc196b45d8c21cd4eff4fd6b6c98f1ce7
conor-ob <conor.w.ob@gmail.com>
2025-12-06 18:53:32 +0000

    fmt lint

--- packages/events/2025/src/3/solution.ts
+++ packages/events/2025/src/3/solution.ts
@@ -3,7 +3,7 @@
  */
 export function part1(input: string) {
   return input.split("\n").reduce((acc, line) => {
-    let max = 0
+    let max = 0;
     for (let i = 0; i < line.length; i++) {
       for (let j = i + 1; j < line.length; j++) {
         const joltage = Number(`${line[i]}${line[j]}`);
@@ -16,13 +16,16 @@ export function part1(input: string) {
 
 export function part2(input: string) {
   return input.split("\n").reduce((acc, line) => {
-    let joltages = []
-    let remainingJoltages = line
+    const joltages = [];
+    let remainingJoltages = line;
 
     while (joltages.length < 12) {
       for (let i = 9; i >= 0; i--) {
         const index = remainingJoltages.indexOf(i.toString());
-        if (index !== -1 && remainingJoltages.slice(index).length >= 12 - joltages.length) {
+        if (
+          index !== -1 &&
+          remainingJoltages.slice(index).length >= 12 - joltages.length
+        ) {
           joltages.push(i);
           remainingJoltages = remainingJoltages.slice(index + 1);
           break;
@@ -30,6 +33,6 @@ export function part2(input: string) {
       }
     }
 
-    return acc + Number(joltages.join(''));
+    return acc + Number(joltages.join(""));
   }, 0);
 }
