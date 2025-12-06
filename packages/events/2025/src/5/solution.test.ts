c95a0e68d78543cabe291a83c314939bda411ba2
conor-ob <conor.w.ob@gmail.com>
2025-12-06 23:48:29 +0000

    part 1 test

--- packages/events/2025/src/5/solution.test.ts
+++ packages/events/2025/src/5/solution.test.ts
@@ -5,7 +5,7 @@ import { part1, part2 } from "./solution";
 
 describe("day 5", () => {
   test("part 1", () => {
-    expect(part1(readFile("./src/5/input.txt"))).toBe("TODO");
+    expect(part1(readFile("./src/5/input.txt"))).toBe(761);
   });
 
   test("part 2", () => {
