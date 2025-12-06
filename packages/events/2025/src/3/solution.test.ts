4e02131dd2ac13cc7f231067a834702488b3d2f5
conor-ob <conor.w.ob@gmail.com>
2025-12-06 18:50:12 +0000

    feat(2025): day 3

---
+++ packages/events/2025/src/3/solution.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "vitest";
+
+import { readFile } from "@aoc/lib/utils";
+
+import { part1, part2 } from "./solution";
+
+describe("day 3", () => {
+  test("part 1", () => {
+    expect(part1(readFile("./src/3/input.txt"))).toBe(17179);
+  });
+
+  test("part 2", () => {
+    expect(part2(readFile("./src/3/input.txt"))).toBe(170025781683941);
+  });
+});
