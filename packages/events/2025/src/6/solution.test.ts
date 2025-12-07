36636b5397a88d37613c3fc1d49c9d33dbba5396
conor-ob <conor.w.ob@gmail.com>
2025-12-07 14:41:25 +0000

    part 2

--- packages/events/2025/src/6/solution.test.ts
+++ packages/events/2025/src/6/solution.test.ts
@@ -9,6 +9,6 @@ describe("day 6", () => {
   });
 
   test("part 2", () => {
-    expect(part2(readFile("./src/6/input.txt"))).toBe("TODO");
+    expect(part2(readFile("./src/6/input.txt"))).toBe(8486156119946);
   });
 });
